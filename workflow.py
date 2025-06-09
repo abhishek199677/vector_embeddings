073bb3cb4dc211a9878c06b4dff1b8115779083f
abhishek199677 <puttapoguabhishek1007@gmail.com>
2025-06-09 16:14:15 +0530

    workflow diagram

---
+++ workflow.py
@@ -0,0 +1,48 @@
+                                                                            
+                                                                            /* TEXT   
+                                                                                |
+                                                                                |
+                                                                                |
+                                                                            EMBEDDING_VECTOR    # {numerical}
+                                                                                |
+                                                                                | 
+                                                                                |  
+                                                                            EMBEDDING_MODEL  {openai, google}
+                                                                                |
+                                                                                |
+                                                                                |
+                                                                                |
+                                                                            VECTOR_DATABASE  {pinecone, chromadb, waviet}
+                                                                                |
+                                                                                |
+                                                                                |
+                                                                            Search Engine   */
+                                                                            
+                                                                            
+
+
+
+
+
+__________________________________________________________________________________________________________________________________________________________________________________
+___________________________________________________________________________________________________________________________________________________________________________________
+
+
+
+
+
+
+
+                                                                            [1] TEXT (Raw Input)
+                                                                                |
+                                                                                v
+                                                                            [2] EMBEDDING MODEL (e.g., OpenAI, Google, HuggingFace)
+                                                                                |
+                                                                                v
+                                                                            [3] EMBEDDING VECTOR (High-dimensional numerical representation)
+                                                                                |
+                                                                                v
+                                                                            [4] VECTOR DATABASE (e.g., Pinecone, ChromaDB, Weaviate)
+                                                                                |
+                                                                                v
+                                                                            [5] VECTOR SEARCH ENGINE (Similarity Search & Retrieval)
